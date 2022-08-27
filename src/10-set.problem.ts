8a30725ab330d34c79dfdd2fb4a4ea94c3e6d5da
Mert Can Altın <mertgold60@gmail.com>
2022-08-27 08:41:13 +0000

    finished

--- src/10-set.problem.ts
+++ src/10-set.problem.ts
@@ -1,7 +1,8 @@
 import { expect, it } from "vitest";
+import { string } from "zod";
 import { Equal, Expect } from "./helpers/type-utils";
 
-const guitarists = new Set();
+const guitarists = new Set<string>();
 
 guitarists.add("Jimi Hendrix");
 guitarists.add("Eric Clapton");
